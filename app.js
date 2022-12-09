e9c99a8beb2fa4a0b4a0c30cce0c4aaa3409ce1a
Robert Schumacher <ras0219@outlook.com>
2022-12-09 09:28:53 -0800

    Poll for output.json

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use("/reddisk", express.static("/mnt/minecraft/62e65afb-e9d7-46f3-869b-d5324ed52d7a"))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
---
+++ public/js/client.js
@@ -0,0 +1,51 @@
+function Log(props) {
+  const children = []
+  if (typeof props.log == "object") {
+    for (const v of Object.values(props.log)) {
+      children.push(React.createElement('li', null, `${v}`));
+    }
+  }
+  return React.createElement('ul', null, ...children);
+}
+
+function App(props) {
+  const [data, setData] = React.useState(null);
+  const [status, setStatus] = React.useState("init");
+  const timeout = React.useRef(true);
+
+  const updateData = async (controller) => {
+    const signal = controller.signal;
+    setStatus("fetching");
+    const res = await fetch("/reddisk/home/output.json", { signal });
+    try {
+      if (res.status != 200) {
+        setStatus(res.status);
+      } else {
+        const json = await res.json();
+        setData(json);
+        setStatus("loaded");
+      }
+    } catch (e) {
+      setStatus(`${e}`)
+    }
+    if (timeout.current) {
+      timeout.current = setTimeout(() => updateData(controller), 5000)
+    }
+  };
+  React.useEffect(() => {
+    const controller = new AbortController();
+    updateData(controller);
+    return () => {
+      if (timeout.current) clearTimeout(timeout.current);
+      controller.abort();
+    };
+  }, []);
+  return React.createElement('div', null, `Hello ${props.toWhat} - ${status}`,
+    React.createElement(Log, { log: data?.log }),
+    React.createElement('div', null, JSON.stringify(data)));
+}
+
+ReactDOM.render(
+  React.createElement(App, { toWhat: 'world' }),
+  document.getElementById('root')
+);
