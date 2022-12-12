54a59457c1ebdd4d77a63332139f1a599438823b
Robert Schumacher <ras0219@outlook.com>
2022-12-12 02:56:21 +0000

    Add smoother and sort

--- public/js/client.js
+++ public/js/client.js
@@ -8,7 +8,20 @@ function Log(props) {
   return React.createElement('ul', null, ...children);
 }
 
-function Chart({dataY, dataX}) {
+function applyFIR(data, kernel) {
+  const revKernel = [...kernel].reverse();
+  const ret = new Array(data.length).fill(NaN, 0, kernel.length - 1);
+  for (var i = kernel.length - 1; i < data.length; ++i) {
+    var v = 0;
+    for (var k = 0; k < kernel.length; ++k) {
+      v += +revKernel[k] * +data[i - k];
+    }
+    ret[i] = v;
+  }
+  return ret;
+}
+
+function Chart({ dataY, dataX, useDerivative, useSmooth }) {
   const width = 700;
   const height = 350;
   const refSVG = React.useRef();
@@ -22,8 +35,29 @@ function Chart({dataY, dataX}) {
       var I = d3.range(dataX.length);
       const D = d3.map(I, defined);
       I = I.filter(i => D[i]).sort((a, b) => +dataX[a] - +dataX[b]);
+
+      dataY = I.map(i => +dataY[i])
+      dataX = I.map(i => +dataX[i])
+      I = d3.range(dataY.length)
+
+      if (useDerivative) {
+        const newDataY = new Array(dataY.length);
+        newDataY[0] = NaN
+        for (var i = 1; i < dataY.length; ++i) {
+          const j = i - 1;
+          const delta = dataX[i] - dataX[j];
+          newDataY[i] = delta > 0 ? (dataY[i] - dataY[j]) / delta : NaN;
+        }
+        dataY = newDataY;
+      }
+      if (useSmooth) {
+        const newDataY = applyFIR(dataY, [0.05, 0.1, 0.15, 0.2, 0.2, 0.15, 0.1, 0.05]);
+        dataY = newDataY;
+      }
+      I = I.filter(i => !isNaN(dataY[i]))
+
       const yScale = d3.scaleLinear()
-        .domain([0, d3.max(dataY)])
+        .domain(!useDerivative ? [0, d3.max(dataY)] : [Math.min(0, d3.min(dataY)), Math.max(0, d3.max(dataY))])
         .range([height - 40, 20])
 
       const xScale = d3.scaleLinear()
@@ -46,27 +80,37 @@ function Chart({dataY, dataX}) {
         .attr("stroke", "black")
         .attr("d", line(I))
     }
-  }, [dataY, dataX, width, height])
+  }, [dataY, dataX, width, height, useDerivative, useSmooth])
 
-  return React.createElement('svg', {width: '100%', height: '350px', ref: refSVG},
-    React.createElement('g', {ref: refAxisX}),
-    React.createElement('g', {ref: refAxisY}),
-    React.createElement('path', {ref: refPath})
+  return React.createElement('svg', { width: '100%', height: '350px', ref: refSVG },
+    React.createElement('g', { ref: refAxisX }),
+    React.createElement('g', { ref: refAxisY }),
+    React.createElement('path', { ref: refPath })
   );
 }
 
-function AnyChart({charts}) {
+function ComboBox({ options, state }) {
+  const [selected, setSelected] = state;
+  const els = options.map((n) => React.createElement('option', { value: n }, n));
+  return React.createElement('select', {
+    onChange: (x) => { setSelected(x.target.value); },
+    value: selected
+  }, els);
+}
+
+function AnyChart({ charts }) {
   const ch = charts?.d
-  const [selected, setSelected] = React.useState(null);
-  const ref = React.useRef();
+  const selectChart = React.useState(null);
+  const selectFilter = React.useState('none');
   if (!ch) return React.createElement('div');
 
-  const keys = Object.keys(ch)
-  const options = keys.map((n) => React.createElement('option', {value: n}, n))
+  const useDerivative = selectFilter[0] && { none: false, smooth: false, derivative: true, smoothDerivative: true }[selectFilter[0]];
+  const useSmooth = selectFilter[0] && { none: false, smooth: true, derivative: false, smoothDerivative: true }[selectFilter[0]];
 
   return React.createElement('div', null,
-    React.createElement('select', {onChange: (x) => { setSelected(x.target.value); console.log(x.target.value); }, value: selected, ref: ref}, options),
-    React.createElement(Chart, {dataY: ch[selected], dataX: ch.tick})
+    React.createElement(ComboBox, { options: Object.keys(ch).sort(), state: selectChart }),
+    React.createElement(ComboBox, { options: ['none', 'smooth', 'derivative', 'smoothDerivative'], state: selectFilter }),
+    React.createElement(Chart, { dataY: ch[selectChart[0]], dataX: ch.tick, useDerivative, useSmooth })
   );
 }
 
